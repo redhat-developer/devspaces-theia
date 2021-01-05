96a1c306410335155587b264807e0303d57fb789
nickboldt <nboldt@redhat.com>
2021-01-05 15:14:17 -0500

    bump to nodeVersion=12.18.4
    Change-Id: I8abb86b3b8e8d508ddc49dee2fa8f9b6f302c562
    Signed-off-by: nickboldt <nboldt@redhat.com>

--- build.sh
+++ build.sh
@@ -75,7 +75,7 @@ SKIP_SYNC_TESTS=0
 DOCKERFLAGS="" # eg., --no-cache --squash
 PODMAN="" # by default, use docker
 PODMANFLAGS="" # optional flags specific to podman build command 
-nodeVersion="12.18.2" # version of node to use (aligned to version in ubi base images)
+nodeVersion="12.18.4" # version of node to use (aligned to version in ubi base images)
 
 CRW_VERSION="" # must set this via cmdline
 MIDSTM_BRANCH="" # must set this via cmdline
